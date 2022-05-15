ec1c0f3d83c60f43665bac4981e09bf212b84c2a
GengarStackz <solidsoft@protonmail.com>
2022-05-15 14:09:22 +0200

    Published to heroku

--- client/src/app/errors/NotFound.tsx
+++ client/src/app/errors/NotFound.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
     return(
         <Container component={Paper} sx={{height: 400}}>
-            <Typography gutterBottom variant='h3'>Ooops! We looked anywhere and could not find what your are looking for</Typography>\
+            <Typography gutterBottom variant='h3'>Ooops! We looked anywhere and could not find what your are looking for</Typography>
             <Divider/>
             <Button fullWidth component={Link} to='/catalog'>Go back to shop</Button>
         </Container>
